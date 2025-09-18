ac5ea85893ca290141399bbc3f71deb15ca106a3
Ryan <rhurless@gmail.com>
2025-09-18 11:42:09 -0500

    Add files via upload

---
+++ calculator app.py
@@ -0,0 +1,26 @@
+# calculator app
+
+import tkinter as tk
+
+def buttonclick(number):
+
+
+
+window = tk.Tk
+window.title("Calculator")
+
+entry = tk.entry(window, width = 35, borderwidth = 9)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
